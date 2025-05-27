72710517de39d2ccfb099528b0f7160eb3334c8c
Juan Gabriel Pared <138406639+gabo4567@users.noreply.github.com>
2025-05-27 19:21:45 -0300

    Update Script Roles y Usuarios.sql

--- database/Script Roles y Usuarios.sql
+++ database/Script Roles y Usuarios.sql
@@ -3,12 +3,14 @@ DROP USER IF EXISTS 'secretaria'@'localhost';
 DROP USER IF EXISTS 'especialista'@'localhost';
 DROP USER IF EXISTS 'admin'@'localhost';
 DROP USER IF EXISTS 'paciente_web'@'localhost';
+DROP USER IF EXISTS 'tutor'@'localhost';
 
 -- ELIMINACIÓN PREVIA DE ROLES (si existen)
 DROP ROLE IF EXISTS 'rol_secretaria';
 DROP ROLE IF EXISTS 'rol_especialista';
 DROP ROLE IF EXISTS 'rol_administrador';
 DROP ROLE IF EXISTS 'rol_paciente';
+DROP ROLE IF EXISTS 'rol_tutor';
 
 USE clinica;
 -- CREACIÓN DE ROLES
@@ -16,6 +18,7 @@ CREATE ROLE 'rol_secretaria';
 CREATE ROLE 'rol_especialista';
 CREATE ROLE 'rol_administrador';
 CREATE ROLE 'rol_paciente';
+CREATE ROLE 'rol_tutor';
 
 -- ASIGNACIÓN DE PRIVILEGIOS A CADA ROL
 
@@ -41,12 +44,19 @@ ON clinica.consulta_web TO 'rol_paciente';
 GRANT SELECT 
 ON clinica.turno TO 'rol_paciente';
 
+-- Tutor: puede ver turnos y consultar datos relacionados a pacientes a su cargo
+GRANT SELECT 
+ON clinica.turno TO 'rol_tutor';
+GRANT SELECT 
+ON clinica.paciente TO 'rol_tutor';
+
 -- CREACIÓN DE USUARIOS
 
 CREATE USER 'secretaria'@'localhost' IDENTIFIED BY 'clave_segura_secretaria';
 CREATE USER 'especialista'@'localhost' IDENTIFIED BY 'clave_segura_especialista';
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'clave_segura_admin';
 CREATE USER 'paciente_web'@'localhost' IDENTIFIED BY 'clave_segura_paciente';
+CREATE USER 'tutor'@'localhost' IDENTIFIED BY 'clave_segura_tutor';
 
 -- ASIGNACIÓN DE ROLES A USUARIOS
 
@@ -61,6 +71,9 @@ SET DEFAULT ROLE 'rol_administrador' TO 'admin'@'localhost';
 
 GRANT 'rol_paciente' TO 'paciente_web'@'localhost';
 SET DEFAULT ROLE 'rol_paciente' TO 'paciente_web'@'localhost';
+
+GRANT 'rol_tutor' TO 'tutor'@'localhost';
+SET DEFAULT ROLE 'rol_tutor' TO 'tutor'@'localhost';
 
 -- APLICAR CAMBIOS
 FLUSH PRIVILEGES;
