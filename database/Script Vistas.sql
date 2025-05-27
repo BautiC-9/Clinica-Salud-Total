c7f1d569d60e50da0ce2f7d80d7888ee12113325
Juan Gabriel Pared <138406639+gabo4567@users.noreply.github.com>
2025-05-27 19:23:21 -0300

    Update Script Vistas.sql

--- database/Script Vistas.sql
+++ database/Script Vistas.sql
@@ -2,6 +2,7 @@ DROP VIEW IF EXISTS vista_paciente;
 DROP VIEW IF EXISTS vista_profesional;
 DROP VIEW IF EXISTS vista_secretaria;
 DROP VIEW IF EXISTS vista_profesionales_paciente;
+DROP VIEW IF EXISTS vista_paciente_tutor;
 
 -- Vista de pacientes
 USE clinica;
@@ -59,7 +60,6 @@ FROM persona p
 WHERE p.id_rol = (SELECT id_rol FROM rol WHERE nombre = 'Secretaria');
 
 -- Vista de profesionales (consulta desde rol paciente)
-
 CREATE VIEW vista_profesionales_paciente AS
 SELECT 
     p.id_profesional,
@@ -74,4 +74,21 @@ SELECT
 FROM profesional p
 JOIN persona per ON p.id_persona = per.id_persona
 JOIN especialidad e ON p.id_especialidad = e.id_especialidad
+WHERE p.id_estado = 1;  -- Mostrar solo profesionales activos
+
+-- Vista de tutores
+CREATE VIEW vista_paciente_tutor AS
+SELECT 
+    pt.id_paciente, 
+    p.nombre AS nombre_paciente, 
+    p.apellido AS apellido_paciente,
+    t.id_tutor, 
+    per_t.nombre AS nombre_tutor, 
+    per_t.apellido AS apellido_tutor,
+    t.parentesco
+FROM paciente_tutor pt
+JOIN paciente pa ON pt.id_paciente = pa.id_paciente
+JOIN persona p ON pa.id_persona = p.id_persona
+JOIN tutor t ON pt.id_tutor = t.id_tutor
+JOIN persona per_t ON t.id_persona = per_t.id_persona;
 WHERE p.id_estado = 1;  -- Mostrar solo profesionales activos
